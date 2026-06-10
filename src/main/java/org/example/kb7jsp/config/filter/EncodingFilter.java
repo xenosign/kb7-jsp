d9334e40d0a48271f98c781716c9fdfd18831f35
Tetz <xenosign@naver.com>
2026-06-10 13:34:40 +0900

    Update EncodingFilter.java

--- src/main/java/org/example/kb7jsp/config/filter/EncodingFilter.java
+++ src/main/java/org/example/kb7jsp/config/filter/EncodingFilter.java
@@ -5,7 +5,7 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
 
-@WebFilter(urlPatterns ="/*")
+@WebFilter(urlPatterns = "/*")
 public class EncodingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
